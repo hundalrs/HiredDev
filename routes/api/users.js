0ae1542a9228a41b3bc1940e6d7e630301546364
Raman Hundal <rshundal11@gmail.com>
2018-08-20 17:15:04 -0400

    Added initial server side validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
+
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
 
 // Load User model
 const User = require("../../models/User");
@@ -16,9 +20,17 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 // @desc      Regiser user
 // @access    Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json({ errors });
     } else {
       const newUser = new User({
         name: req.body.name,
@@ -79,5 +91,16 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @route     GET api/users/current
+// @desc      Return current user
+// @access    Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json(req.user);
+  }
+);
 
 module.exports = router;
