a935b2dd27360d7e192ac389e81a3a69138b51e9
Raman Hundal <rshundal11@gmail.com>
2018-08-23 20:47:40 -0400

    Got rid of route pertaining to profile

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
 const jobs = require("./routes/api/jobs");
 
 const app = express();
@@ -33,7 +32,6 @@ require("./config/passport")(passport);
 
 // Use Routes
 app.use("/api/users", users);
-app.use("/api/profile", profile);
 app.use("/api/jobs", jobs);
 
 const port = process.env.PORT || 5000;
