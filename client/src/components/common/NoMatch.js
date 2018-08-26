973141fadbf13d08b5bb736c82cb37ae5ce9a7f1
Raman Hundal <rshundal11@gmail.com>
2018-08-26 02:04:07 -0400

    Created a 404 message if resource cannot be reached

--- client/src/components/common/NoMatch.js
+++ client/src/components/common/NoMatch.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
+import "../../styles/nomatch.css";
+
 export default class NoMatch extends Component {
   render() {
     return (
       <div>
-        <h2 className="mt-4 mb-4">This is not a valid url</h2>
+        <h2 className="mt-4 mb-4 notfound text-center">404</h2>
       </div>
     );
   }
