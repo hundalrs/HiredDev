3caf890d71405e6f22d1b0fb77005414354401a9
Raman Hundal <rshundal11@gmail.com>
2018-09-17 02:06:47 -0400

    Got rid of an unnecessary import

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Redirect, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
